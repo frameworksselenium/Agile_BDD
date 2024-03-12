26c908b9124aa3282c4ef0ded549627018c42345
frameworksselenium <frameworksselenium@gmail.com>
2024-03-12 10:28:51 -0400

    changes

---
+++ src/main/java/com/open/hotel/webDriverFactory/BrowserStackRemoteDriverFactory.java
@@ -0,0 +1,53 @@
+package com.open.hotel.webDriverFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BrowserStackRemoteDriverFactory {
+    private static BrowserStackRemoteDriverFactory instance = new BrowserStackRemoteDriverFactory();
+
+    private BrowserStackRemoteDriverFactory() {
+    }
+
+    public static BrowserStackRemoteDriverFactory getInstance() {
+        return instance;
+    }
+
+    public WebDriver createNewDriver(String browser, String platformName, String RemoteURL) {
+
+        RemoteWebDriver driver = null;
+        if (browser.toUpperCase().contains("CH")) {
+            ChromeOptions browserOptions = new ChromeOptions();
+            browserOptions.setPlatformName("windows");
+            browserOptions.setBrowserVersion("latest");
+            try {
+                URL url = new URL(RemoteURL);
+                driver = new RemoteWebDriver(url, browserOptions);
+                driver.manage().window().maximize();
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (browser.toUpperCase().contains("SF")) {
+            SafariOptions safariOptions = new SafariOptions();
+            safariOptions.setPlatformName("mac");
+            safariOptions.setBrowserVersion("latest");
+            try {
+                URL url = new URL(RemoteURL);
+                driver = new RemoteWebDriver(url, safariOptions);
+                driver.manage().window().maximize();
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return driver;
+    }
+
+}
